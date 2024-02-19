6e13430d910547f7b6fca414905e1660b804a838
dinhlong <longtd.22ite@vku.udn.vn>
2024-02-19 21:28:32 +0700

    update 19/02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,22 @@ export class AppComponent {
   list: any[] = [];
   task: string = '';
   date: string = '';
+  taskCount: number;
+
+  constructor() {
+    this.list = JSON.parse(localStorage.getItem('tasks') || '[]');
+  }
+  ngOnInit() {
+    this.list = JSON.parse(localStorage.getItem('tasks') || '[]');
+  }
 
   addTask(task: string, date: string) {
     if (!task || !date) {
       alert('Please enter task and date.');
     } else {
-      this.list.push({ id: this.list.length, name: task, date: date });
+      const newId = this.generateUniqueId();
+      this.list.push({ id: newId, name: task, date: date });
+      localStorage.setItem('tasks', JSON.stringify(this.list));
       console.warn(this.list);
       this.task = "";
       this.date = "";
@@ -30,5 +40,9 @@ export class AppComponent {
   removeTask(id: number) {
     console.warn(id);
     this.list = this.list.filter(item => item.id !== id);
+    localStorage.setItem('tasks', JSON.stringify(this.list));
+  }
+  generateUniqueId(): number {
+    return new Date().getTime();
   }
 }
